56b1ec1c8269df0364ccd13155946f0089c83ae7
Samber9891 <Slem0119@gmail.com>
2020-10-27 09:19:41 -0400

    git init

---
+++ techtalentwitter/src/main/java/com/tts/techtalentwitter/TechtalentwitterApplication.java
@@ -0,0 +1,13 @@
+package com.tts.techtalentwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtalentwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtalentwitterApplication.class, args);
+	}
+
+}
---
+++ techtalentwitter/src/test/java/com/tts/techtalentwitter/TechtalentwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.techtalentwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtalentwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
