067e1ac9279b666c99841adfc38e7f751539465c
Samber9891 <Slem0119@gmail.com>
2020-10-28 17:13:58 -0400

    added email, length, and not empty messages

--- techtalentwitter/src/main/java/com/tts/techtalentwitter/model/User.java
+++ techtalentwitter/src/main/java/com/tts/techtalentwitter/model/User.java
@@ -1,8 +1,8 @@
 package com.tts.techtalentwitter.model;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
-
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -13,10 +13,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-
-
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 
 @Entity
@@ -27,11 +29,24 @@ public class User {
     @Column(name="user_id")
     private Long id;
 
+    @Email(message = "Please provide a valid email")
+    @NotEmpty(message = "Please provide an email")
     private String email;
+
+    @Length(min = 3, message = "Your username must have at least 3 characters")
+    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+    @Pattern(regexp="[^\\s]+", message="Your username cannot contain spaces")
     private String username;
+ 
+    @Length(min = 5, message = "Your password must have at least 5 characters")
     private String password;
+  
+    @NotEmpty(message = "Please provide your first name")
     private String firstName;
+ 
+    @NotEmpty(message = "Please provide your last name")
     private String lastName;
+   
     private int active;
 
     @CreationTimestamp
@@ -42,22 +57,41 @@ public class User {
 inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_follow", joinColumns = @JoinColumn(name = "user_id"), 
+inverseJoinColumns = @JoinColumn(name = "follower_id"))
+    private List<User> followers;
+
+    @ManyToMany(mappedBy="followers")
+    private List<User> following;
+
+
 public User() {
 
 }
 
-public User(Long id, String email, String username, String password, String firstName, String lastName, int active,
-		Date createdAt, Set<Role> roles) {
-	this.id = id;
-	this.email = email;
-	this.username = username;
-	this.password = password;
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.active = active;
-	this.createdAt = createdAt;
-	this.roles = roles;
+public User(Long id,
+        @Email(message = "Please provide a valid email") @NotEmpty(message = "Please provide an email") String email,
+        @Length(min = 3, message = "Your username must have at least 3 characters") @Length(max = 15, message = "Your username cannot have more than 15 characters") @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces") String username,
+        @Length(min = 5, message = "Your password must have at least 5 characters") String password,
+        @NotEmpty(message = "Please provide your first name") String firstName,
+        @NotEmpty(message = "Please provide your last name") String lastName, int active, Date createdAt,
+        Set<Role> roles, List<User> followers, List<User> following) {
+    this.id = id;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.active = active;
+    this.createdAt = createdAt;
+    this.roles = roles;
+    this.followers = followers;
+    this.following = following;
 }
+
+
+
 
 public Long getId() {
     return id;
@@ -130,6 +164,22 @@ public Set<Role> getRoles() {
 public void setRoles(Set<Role> roles) {
 	this.roles = roles;
 }
+public List<User> getFollowers() {
+    return followers;
+}
+
+public void setFollowers(List<User> followers) {
+    this.followers = followers;
+}
+
+
+public List<User> getFollowing() {
+    return following;
+}
+
+public void setFollowing(List<User> following) {
+    this.following = following;
+}
 
 @Override
 public String toString() {
@@ -140,4 +190,16 @@ public String toString() {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
