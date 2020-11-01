d25335d029cdcbc77062f0aaba4bbba04ee9e7c5
Samber9891 <Slem0119@gmail.com>
2020-11-01 15:34:45 -0500

    added findby methods

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostRepository.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostRepository.java
@@ -1,7 +1,15 @@
 package com.tts.techtalentblog.BlogPost;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
+
+	List<BlogPost> findByTags_PhraseOrderByCreatedAtDesc(String tag);
+
+	List<BlogPost> findAllByOrderByCreatedAtDesc();
     
 }
